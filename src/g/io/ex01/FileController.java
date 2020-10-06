03f9eec2d977c012d49f20af1ff36131a44a7ac5
Joowon Jung <jjoowon.jung@gmail.com>
2020-10-07 00:15:09 +0900

    20-10-07

--- src/g/io/ex01/FileController.java
+++ src/g/io/ex01/FileController.java
@@ -1,21 +1,29 @@
 // 1006 과제 첫번째 장
 package g.io.ex01;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 /*
  * 	파일을 하나 만들고, 파일 내용을 읽고, 파일 내용을 출력
- *  거꾸로 문자열 조작 => 읽어서 어디에 담고 출력해야 할 듯 ?
+ *  거꾸로 문자열 조작 => 파일내용을 읽어서 어디에 담고 출력해야 할 듯 ? 
+ *  => ArrayList에 담았음 ! List를 이용하여 Vector, ArrayList 사용하면 편리 (다형성)
  */
 public class FileController {
 
 	///main method
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception { // try - catch 안 하고 그냥 던짐!
 		FileController c = new FileController();
-		String oldf = "/Users/jungjoowon/eclipse-workspace/JavaStudy/src/g/io/ex01/oldf.txt";
-		String newf = "/Users/jungjoowon/eclipse-workspace/JavaStudy/src/g/io/ex01/newf.txt";
+		String oldf = "oldf.txt";
+		             // /Users/jungjoowon/eclipse-workspace/JavaStudy/src/g/io/ex01/oldf.txt => 절대경로
+					 // 이클립스는 default가 자바 프로젝트 안이니까 oldf.txt만 하면 자바 프로젝트 안의 파일이라는 뜻!
+		String newf = "newf.txt";
 		c.reverseLine(oldf, newf);
 	}
 	
-	// String 타입의 텍스트 형식 파일명을 입력받아 텍스트의 라인 순서를 반대로 하여
+	// String 타입의 텍스트 형식 파일명을 입력받아 텍스트의 라인 순서를 '반대로' 하여
 	// 두 번째 인자인 출력할 String 타입의 텍스트 파일명으로 저장
 	// 즉. 입력라인의 첫번째 라인은 출력라인의 제일 마지막 라인이 됨 !
 	public void reverseLine(String inputFileName, String outputFileName) throws Exception {
@@ -23,19 +31,47 @@ public class FileController {
 		// ==> 원본파일 Data를 read (받아옴) 할 수 있는 BufferedReader 생성
 		BufferedReader br = new BufferedReader(new FileReader(inputFileName));
 		
-		// ==> Data를 write (내보냄) 할 수 있는 BufferedWriter 생성
-		BufferedWriter bw = new BufferedWriter(new FileWriter(outputFileName));
+		// Data read 한 것을 ArrayList에 담기 위해 선언 !
+		List <String> m = new ArrayList<String>(); // Vector 써도 상관 x !
+		// ArrayList <String> m = new ArrayList<String>();
 		
-		// read / writer 하는 while 문
-		String source = null; // source : 파일 안의 내용들.
-		while ( ( source = br.readLine()) != null ) { 
-			bw.write(source + '\n'); // 개행해서 출력해주기 위함
-			//bw.newLine(); 
+		String line = null; // 파일 안의 내용들 
+		while ( (line = br.readLine() ) != null ) { // 내용이 끝날 때 까지 
+			m.add(line); // 한 줄씩 ArrayList에 저장 
 		}
-		bw.flush();
+		br.close(); // BufferedReader 다 썼으니 닫아줌
 		
-		br.close();
-		bw.close();
+//		FileOutputStream fos = new FileOutputStream(outputFileName);
+//		PrintWriter pw = new PrintWriter(fos);
+		PrintWriter pw = new PrintWriter(new FileOutputStream(outputFileName));
+		for (int i = m.size()-1; i >= 0; i--) { // 한 줄씩 ArrayList에 저장한 것을 거꾸로 가져옴 
+			pw.println(m.get(i));
+		}
+		pw.flush();
+		pw.close();
+		
+		// *** PrintWriter vs BufferedWriter ***
+		// 자바에서 파일에다가 출력을 할 때 PrintWriter, BufferedWriter을 이용할 수 있음
+		// - PrintWriter : print, println 등 다양한 출력함수를 제공함으로써 파일 출력 편함
+		// - BufferedWriter : 버퍼를 사용하여 속도 향상! 
+		// ( 메모리 버퍼에 데이터를 보내고, 데이터가 쌓이기를 기다렸다가 한꺼번에 출력 )
+		// 둘 중 하나를 잘 선택해서 사용하면 되는데 이 문제의 경우에는 for문을 사용해서 거꾸로 출력해야 하니까
+		// PrintWriter을 선택하여 쓰는 것 ! 각각의 장점 활용하여 선택 사용하기
+		
+//		// ==> Data를 write (내보냄) 할 수 있는 BufferedWriter 생성
+//		BufferedWriter bw = new BufferedWriter(new FileWriter(outputFileName));
+//		
+//		// read / writer 하는 while 문
+//		String source = null; // source : 파일 안의 내용들.
+//		while ( ( source = br.readLine()) != null ) { 
+//			bw.write(source + '\n'); // 개행해서 한 줄씩 출력해주기 위함
+//			//bw.newLine(); 
+//		}
+//		bw.flush();
+//		
+//		br.close();
+//		bw.close();
+		
 		System.out.println("정상 실행!");
 	}
 }
--- src/g/io/ex02/FileCompareUtil.java
+++ src/g/io/ex02/FileCompareUtil.java
@@ -1,13 +1,46 @@
 // 1006 과제 두번째 장 
 package g.io.ex02;
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+// 파일 두개 만들기 !
+// 첫번째 두번째 파일내용 비교해서 똑같지 않은 것을 ArrayList에 담아서 출력 -> I/O 사용
 
 public class FileCompareUtil {
 
 	// String 타입의 텍스트 형식 파일명 2개를 입력받아 두 텍스트 파일의 내용을 라인별로 비교하고
 	// 차이점이 있는 경우, 해당 라인들을 ArrayList에 담아 리턴
+	
+	// - 두 텍스트 파일의 전체 라인수는 동일하다라고 간주
+	// - 첫번째 입력된 텍스트 파일 내용을 기준으로 두번째 입력된 텍스트 파일 내용을 라인별로 비교하고
+	// 동일할 경우 다음 라인으로 이동
+	// 다를 경우에는 해당 라인의 번호와 함께 두번째 파일의 해당 라인 내용을 결과 용도의 ArrayList에 추가
 	public ArrayList compareFile(String fstFileName, String scdFileName) throws Exception {
+		
+//		FileReader fr = new FileReader(fstFileName); // 텍스트 파일을 읽어들일 때 사용하는 문자 기반 스트림
+//													 // read() 사용해서 한 문자씩 읽음 => 효율성 떨어짐 
+//		BufferedReader br = new BufferedReader(fr); // 문자 입력 스트림에 연결되어 버퍼를 제공해주는 스트림
+//													// readLine() 사용해서 한 줄씩 읽음
+		BufferedReader fstBr = new BufferedReader(new FileReader(fstFileName)); // 첫번째 파일내용 읽음 
+		BufferedReader scdBr = new BufferedReader(new FileReader(scdFileName)); // 두번째 파일내용 읽음 
+		
+		String fstLine = fstBr.readLine();
+		String scdLine = scdBr.readLine();
+		
+		int lineNum = 0;
+		
+		// 읽은 내용을 저장하기 위한 
+		ArrayList <String> resultList = new ArrayList<String> ();
+		
+		while (fstLine != null) {
+			
+			lineNum++;
+			if (!fstLine.contentEquals(scdLine)) {
+				resultList.add("LINE " + lineNum + ":" + scdLine);
+			}
+		}
+		
+		
 		
 		return null;
 	}
