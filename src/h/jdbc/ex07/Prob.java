bcecdcb49d365a1955f99d00b0b4a477153e098c
Joowon Jung <jjoowon.jung@gmail.com>
2020-10-19 22:57:43 +0900

    20-10-19

---
+++ src/h/jdbc/ex07/Prob.java
@@ -0,0 +1,14 @@
+// 1015 과제 07-3
+package h.jdbc.ex07;
+
+public class Prob {
+
+	private static void printEmployeeList(String cityName, String deptName) throws Exception {
+		
+	}
+	
+	public static void main(String[] args) throws Exception {
+		printEmployeeList("lon", "resource");
+	}
+
+}
